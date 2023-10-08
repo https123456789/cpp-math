0bbe1f0246c352a566dde8f759122d21557341d0
https123456789 <81256789+https123456789@users.noreply.github.com>
2023-10-08 00:44:41 +0000

    feat: add basic expression class

--- include/cpp-math/cpp-math.hpp
+++ include/cpp-math/cpp-math.hpp
@@ -1,4 +1,72 @@
+#include <initializer_list>
+#include <typeinfo>
+#include <vector>
+
 #ifndef _CPP_MATH_CPP_MATH_HPP_
 #define _CPP_MATH_CPP_MATH_HPP_
+
+namespace cpp_math {
+
+    enum Operation {
+        MATHOP_ADD,
+        MATHOP_SUB,
+        MATHOP_MULT,
+        MATHOP_DIV,
+        MATHOP_NONE
+    };
+
+    template<typename T>
+    class Primitive {
+        public:
+            Primitive(T value): value(value) {}
+
+            T value;
+    };
+
+    class Expression;
+
+    class ExpressionOperand {
+        public:
+            ExpressionOperand(Primitive<int> p);
+            ExpressionOperand(Expression *e);
+            ExpressionOperand(std::nullptr_t null_value);
+
+            bool operator==(const ExpressionOperand& rhs) const;
+
+        private:
+            void *value;
+            const std::type_info *stored_type;
+    };
+
+    // Expression
+    // Combines primitives and other expressions together
+    // EX:
+    // Primitive + Primitive
+    // Expression + Expression
+    // Primitive + Expression
+    class Expression {
+        public:
+            Expression(
+                ExpressionOperand left,
+                ExpressionOperand right,
+                Operation op
+            );
+        
+            ExpressionOperand left;
+            ExpressionOperand right;
+            Operation operation;
+    };
+    
+    /*** PLACEHOLDER DEFINITION ***/
+    /* There is no actual implementation for this class */
+    class Polynomial {
+        public:
+            Polynomial(int n, std::initializer_list<int> a);
+            
+            int n;
+            std::vector<int> a;
+    };
+    
+}
 
 #endif  // _CPP_MATH_CPP_MATH_HPP_
--- src/expression.cpp
+++ src/expression.cpp
@@ -1,1 +1,44 @@
+#include <stdexcept>
+#include <typeinfo>
 #include <cpp-math/cpp-math.hpp>
+
+using cpp_math::Expression;
+using cpp_math::ExpressionOperand;
+using cpp_math::Operation;
+using cpp_math::Primitive;
+
+Expression::Expression(
+    ExpressionOperand left,
+    ExpressionOperand right,
+    Operation op
+) : left(left),
+    right(right),
+    operation(op)
+{ }
+
+ExpressionOperand::ExpressionOperand(Primitive<int> p) {
+    value = (void*) &p;
+    stored_type = &typeid(p);
+}
+
+ExpressionOperand::ExpressionOperand(Expression *e) {
+    value = (void*) e;
+    stored_type = &typeid(e);
+}
+
+ExpressionOperand::ExpressionOperand(std::nullptr_t null_value) {
+    if (null_value != nullptr) {
+        throw std::invalid_argument("Argument must a null pointer");
+    }
+
+    value = nullptr;
+    stored_type = &typeid(null_value);
+}
+
+bool ExpressionOperand::operator==(const ExpressionOperand& rhs) const {
+    if (*(this->stored_type) != *(rhs.stored_type)) {
+        return false;
+    }
+
+    return true;
+}
